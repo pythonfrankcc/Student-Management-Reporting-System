be4a419117b594246f36ede81dda44b2f12988e2
pythonfrankcc <francismurithi475@gmail.com>
2022-09-27 04:56:41 +0300

    first commit

---
+++ READme.md
@@ -0,0 +1,1 @@
+Currently this deals with your PCs file system.It isimportant to note some of the changes done with type casting, ie, reinterpret_cast may be system specific
---
+++ declarations.h
@@ -0,0 +1,6 @@
+#pragma once
+void create_student();
+void display_sp(int);//display particular record
+void display_all(); // display all records
+void delete_student(int);//delete particular record
+void change_student(int);//edit particular record
